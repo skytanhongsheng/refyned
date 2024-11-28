6e3100dc2338e97934cdf76c7ab930d3f0dd0334
Tirta Swari <tirtasafariman@gmail.com>
2024-11-28 17:33:00 +0800

    feat(seeds): remove bookmarked validation from card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,10 @@
 class Card < ApplicationRecord
   belongs_to :template
+  belongs_to :lesson
   has_many :options, dependent: :destroy
 
   has_one_attached :picture
   has_one_attached :audio
 
-  validates :bookmarked, :instruction, :answer, presence: true
+  validates :instruction, :answer, presence: true
 end
