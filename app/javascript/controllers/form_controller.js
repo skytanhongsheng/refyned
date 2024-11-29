ac75d18f4ce4de51e83f151681040421b154c7df
Hong Sheng Tan <skytanhongsheng@gmail.com>
2024-11-29 13:44:04 +0800

    feat(form): completed adding a curriculum form

--- app/javascript/controllers/form_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="form"
-export default class extends Controller {
-  connect() {
-    // this.currentlayout = 1;
-  }
-}
