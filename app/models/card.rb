9c520c9c4907be096e9f81103c23a5f604f09fc3
Tirta Swari <tirtasafariman@gmail.com>
2024-12-02 14:47:54 +0800

    feat(partials): create partials of templates

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,4 +7,10 @@ class Card < ApplicationRecord
   has_one_attached :audio
 
   validates :instruction, :answer, presence: true
+
+  def template_name
+    # find the name of the template
+    # convert it to lower_snake_case
+    # i.e Picture Comprehension => picture_comprehension
+  end
 end
