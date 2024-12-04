7ef6a6bc5aa738520fb524948b4d5d7bf0f64d3e
Hong Sheng Tan <skytanhongsheng@gmail.com>
2024-12-04 15:03:06 +0800

    remove status column from lessons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_060757) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_070044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,7 +100,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_060757) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "order"
-    t.string "status"
     t.index ["curriculum_id"], name: "index_lessons_on_curriculum_id"
   end
 
