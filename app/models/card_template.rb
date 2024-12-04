7fa5c57576cf7c1b422d4862e42a21d84c550278
Tirta Swari <tirtasafariman@gmail.com>
2024-12-04 17:52:10 +0800

    feat(validations): added inclusion validation to card template name

--- app/models/card_template.rb
+++ app/models/card_template.rb
@@ -1,5 +1,10 @@
 class CardTemplate < ApplicationRecord
   has_many :cards, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, inclusion: { in: [
+    "Picture Comprehension",
+    "Listening Comprehension",
+    "MCQ",
+    "Translate"
+  ] }
 end
